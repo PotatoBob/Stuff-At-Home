7d74ed5cba0cd92961e8c81c25b9df1374f2917b
PotatoBob <jangwoo24@gmail.com>
2016-08-11 15:43:41 -0700

    Date Calculator

---
+++ Formulae/src/CheckMath.java
@@ -0,0 +1,11 @@
+
+public class CheckMath {
+	int ans = 0;
+	public static void main(String[] args) {
+		CheckMath CM = new CheckMath();
+	}
+	CheckMath() {
+		
+		System.out.println((int) 11/6);
+	}
+}
